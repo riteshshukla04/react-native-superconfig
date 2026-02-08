7bcaa7e94ee3b2529a159418f8577632a8fb5b2c
Patrick Kabwe <patrickckabwe@gmail.com>
2026-02-08 21:55:27 +0200

    Feat/type safe config (#4)
    * feat: auto-generate type-safe config from `.env`
    
    * refactor: remove redundant singleton guard in favor of ES module caching

--- superconfig/src/specs/superconfig.nitro.ts
+++ superconfig/src/specs/superconfig.nitro.ts
@@ -1,20 +1,10 @@
-// TODO: Export specs that extend HybridObject<...> here
-
-
 import { NitroModules, type HybridObject } from 'react-native-nitro-modules'
 
-
-
 export interface Config extends HybridObject<{ android: 'c++', ios: 'c++' }> {
-    Config: Record<string, string>
+  Config: Record<string, string>
 }
-const nitroConfig = NitroModules.createHybridObject<Config>("Config")
+const nitroConfig = NitroModules.createHybridObject<Config>('Config')
 
-
-let Config:Record<string, string> | undefined = undefined
-
-if(!Config){
-    Config = nitroConfig.Config as Record<string, string>
-}
+const Config = nitroConfig.Config
 
 export default Config
