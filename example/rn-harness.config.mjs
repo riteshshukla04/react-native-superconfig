acfa4da6f883c555e9a30d0b97367e7979b1932c
riteshshukla04 <riteshshukla2381@gmail.com>
2026-02-09 00:37:36 +0530

    feat: readme

---
+++ example/rn-harness.config.mjs
@@ -0,0 +1,29 @@
+import {
+  androidPlatform,
+  androidEmulator,
+} from '@react-native-harness/platform-android';
+import {
+  applePlatform,
+  appleSimulator,
+} from '@react-native-harness/platform-apple';
+import { webPlatform, chromium } from '@react-native-harness/platform-web';
+
+const config = {
+  entryPoint: './index.js',
+  appRegistryComponentName: 'example',
+
+  runners: [
+    androidPlatform({
+      name: 'android',
+      device: androidEmulator('Pixel_6_Pro'), // Your Android emulator name
+      bundleId: 'com.example', // Your Android bundle ID
+    }),
+    applePlatform({
+      name: 'ios',
+      device: appleSimulator('iPhone 17', '26.2'), // Your iOS simulator name and version
+    bundleId: 'org.reactjs.native.example.example', // Your iOS bundle ID
+    })
+  ],
+};
+
+export default config;
